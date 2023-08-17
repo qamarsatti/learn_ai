ccf4d3bfd3a348920f32fcafcf182d859862c5d8
qamarsatti <qamarhayyatsatti@gmail.com>
2023-08-17 15:13:07 +0500

    add deep learining

---
+++ DeepLearining/transferLearing/feature_extraction.py
@@ -0,0 +1,2 @@
+#imagenet dataset are already train on image classificaion 
+#in feature extraction we will freeze all convocation layer and train own image on dense layer
---
+++ DeepLearining/transferLearing/fine_tuning.py
@@ -0,0 +1,2 @@
+#in there we can retrain and unfreeze some last convocation layer
+#finetuning apply when dataset classificaion lable are diffrent from our project
